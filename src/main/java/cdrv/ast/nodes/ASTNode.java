f045ef24f9771dddcd4f4014dd95642eb6d8d504
Danison Nuñez <danisonnunez001@gmail.com>
2025-11-01 00:16:09 +0800

    Create ASTNode.java

---
+++ src/main/java/cdrv/ast/nodes/ASTNode.java
@@ -0,0 +1,14 @@
+// ASTNode.java
+package cdrv.ast.nodes;
+
+public abstract class ASTNode {
+    protected int line = -1;
+    protected int column = -1;
+    
+    public int getLine() { return line; }
+    public int getColumn() { return column; }
+    public void setSourcePosition(int line, int column) {
+        this.line = line;
+        this.column = column;
+    }
+}
