655e9309ee7c9c35667407e51b616e2b67b5f35d
Danison Nuñez <danisonnunez001@gmail.com>
2025-12-15 13:51:42 +0800

    Add files via upload

---
+++ src/main/java/cod/error/ParseError.java
@@ -0,0 +1,24 @@
+package cod.error;
+
+public class ParseError extends RuntimeException {
+    private int line;
+    private int column;
+    
+    public ParseError(String message, int line, int column) {
+        super(message + " at line " + line + ":" + column);
+        this.line = line;
+        this.column = column;
+    }
+    
+    public ParseError(String message) {
+        super(message);
+    }
+    
+    public int getLine() {
+      return line;
+    }
+    
+    public int getColumn() {
+      return column;
+    }
+}
