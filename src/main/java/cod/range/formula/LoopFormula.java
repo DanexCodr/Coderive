5f65c543e905e3971e650521bc2c42878340566c
Danison Nuñez <danisonnunez001@gmail.com>
2026-01-05 11:37:47 +0800

    Add files via upload

---
+++ src/main/java/cod/range/formula/LoopFormula.java
@@ -0,0 +1,23 @@
+package cod.range.formula;
+
+import cod.ast.nodes.ExprNode;
+import cod.ast.nodes.*;
+import java.util.Map;
+
+public class LoopFormula {
+    public final long start;
+    public final long end;  // inclusive
+    public final ExprNode formula;
+    public final String indexVar;
+    
+    public LoopFormula(long start, long end, ExprNode formula, String indexVar) {
+        this.start = start;
+        this.end = end;
+        this.formula = formula;
+        this.indexVar = indexVar;
+    }
+    
+    public boolean contains(long index) {
+        return index >= start && index <= end;
+    }
+}
